b8f388144d31255e9903391e467fb5981b58c467
jenish-rudani <jrudani1999@gmail.com>
2022-07-31 03:04:32 -0700

    formatting updated

--- fft/DSP_Project_1.ino
+++ fft/DSP_Project_1.ino
@@ -1,18 +1,17 @@
-#include "./fftAlgorithm.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "./fftAlgorithm.h"
 char printBuffer[300];
-void setup()
-{
-  Serial.begin(115200); // Using Serial Port
+void setup() {
+  Serial.begin(115200);  // Using Serial Port
   Serial.println();
   Serial.println();
   Serial.println();
   Serial.print("Starting 4-Point FFT\n");
   int N = 4;
   float inputData[] = {3, 0, 4, 0, 5, 0, 6, 0};
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     Serial.print(inputData[(i * 2)]);
     Serial.print(" + ");
     Serial.print(inputData[(i * 2) + 1]);
@@ -20,13 +19,10 @@ void setup()
   }
   float outputData[8];
   fourPointFFT(inputData, 2, outputData, 2);
-  for (int k = 0; k < 8; k += 2)
-  {
+  for (int k = 0; k < 8; k += 2) {
     sprintf(printBuffer, "%.2f% +.2fi , ", outputData[k], outputData[k + 1]);
     Serial.print(printBuffer);
   }
   Serial.println("\n\nFinished Calculating FFT");
 }
-void loop()
-{
-}
+void loop() {}
